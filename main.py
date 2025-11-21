eb091ca8f86ac6e2e6bed60721642a1beddccb91
etekmen13 <etekmen13@gmail.com>
2025-11-21 14:35:26 -0500

    folder structure and toml (uv)

---
+++ main.py
@@ -0,0 +1,9 @@
+from typing import Optional, List, Literal
+from pydantic import BaseModel, Field, PositiveFloat, PositiveInt
+
+def main():
+    print("Hello from pgem!")
+
+
+if __name__ == "__main__":
+    main()
